b9d382c7ccfb21d5b45e6b7d8853b2a0e8ffa2ac
krishna9304 <krishhtrishh9304@gmail.com>
2021-09-08 14:59:39 +0530

    backend url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,8 @@
+const isDev = process.env.NODE_ENV !== "production";
 module.exports = {
-  BACKEND_URL: `http://localhost:8080/`,
+  BACKEND_URL: isDev
+    ? `http://localhost:8080/`
+    : "https://road-rakshak-server.herokuapp.com/",
   MAPBOX_API_KEY:
     "pk.eyJ1Ijoibm90LXZhbGlkIiwiYSI6ImNrbGt1M2ZiMTEwaDMycG5tbDhseTY5YmoifQ.j0DITrdH06LMzgQ4A-H5vg",
 };
